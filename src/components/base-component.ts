d0a7e013e92a19ed7d57f988360567ab59ca2349
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-05-31 11:14:06 +0200

    using namespace to import files

---
+++ src/components/base-component.ts
@@ -0,0 +1,41 @@
+// Component Base Class
+namespace App {
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      instertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementId)! as T;
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedNode.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+      this.attach(instertAtStart);
+    }
+
+    private attach(instertAtBeginning: boolean) {
+      this.hostElement.insertAdjacentElement(
+        instertAtBeginning ? "afterbegin" : "beforeend",
+        this.element
+      );
+    }
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
